2f8a2bd66e7bab5eb69c27633ea649212ed76b4f
DESKTOP-RNCUD5T\xushuguang <>
2023-07-28 16:30:13 +0800

    初始化代码

---
+++ src/api/desk/notice.js
@@ -0,0 +1,49 @@
+import request from '@/axios';
+
+export const getList = (current, size, params) => {
+  return request({
+    url: '/blade-desk/notice/list',
+    method: 'get',
+    params: {
+      ...params,
+      current,
+      size,
+    },
+  });
+};
+
+export const remove = ids => {
+  return request({
+    url: '/blade-desk/notice/remove',
+    method: 'post',
+    params: {
+      ids,
+    },
+  });
+};
+
+export const add = row => {
+  return request({
+    url: '/blade-desk/notice/submit',
+    method: 'post',
+    data: row,
+  });
+};
+
+export const update = row => {
+  return request({
+    url: '/blade-desk/notice/submit',
+    method: 'post',
+    data: row,
+  });
+};
+
+export const getNotice = id => {
+  return request({
+    url: '/blade-desk/notice/detail',
+    method: 'get',
+    params: {
+      id,
+    },
+  });
+};
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig, loadEnv } from 'vite';
+
+const { resolve } = require('path');
+import createVitePlugins from './vite/plugins';
+// https://vitejs.dev/config/
+export default ({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd());
+  const { VITE_APP_BASE } = env;
+  return defineConfig({
+    base: VITE_APP_BASE,
+    server: {
+      port: 2888,
+      proxy: {
+        '/api': {
+          // target: 'http://localhost',
+          target: 'http://dp.nustantai.com',
+          //target: 'http://test3.javablade.com',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '~': resolve(__dirname, './'),
+        '@': resolve(__dirname, './src'),
+        components: resolve(__dirname, './src/components'),
+        styles: resolve(__dirname, './src/styles'),
+        utils: resolve(__dirname, './src/utils'),
+      },
+    },
+    plugins: createVitePlugins(env, command === 'build'),
+  });
+};
